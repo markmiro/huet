ff39e6e932609183ec2c5e2ff37df4c50ea71e7c
Mark Mironyuk <contact@markmiro.com>
2018-12-12 18:04:05 -0800

    useDebounce

--- src/ExampleColorContrast.js
+++ src/ExampleColorContrast.js
@@ -1,35 +1,40 @@
 import React from "react";
 import huet from "./huet";
 import Contrast from "./Contrast";
+import { useDebounce } from "use-debounce";
 
 export default function ColorContrast() {
   const ctx = huet.useTheme();
+  const debouncedCtx = useDebounce(ctx.contextValue, 200);
   const graySteps = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
   // const graySteps = [0, 4, 8, 16, 32, 64, 100];
   const colorSteps = [0, 4, 8, 16, 32, 64, 100];
+
   return (
-    <div>
-      {graySteps.map(grayStep => (
-        <Contrast key={grayStep} bg={grayStep} className="pa2">
-          <b>{grayStep}</b>
-          <div className="flex items-center flex-wrap">
-            {Object.keys(ctx.value.ramps).map(ramp => (
-              <div key={ramp} className="flex mt1 mr2">
-                {colorSteps.map(colorStep => (
-                  <Contrast
-                    key={colorStep}
-                    bg={colorStep}
-                    bgRamp={ramp}
-                    className="pa1"
-                  >
-                    {colorStep.toString()}
-                  </Contrast>
-                ))}
-              </div>
-            ))}
-          </div>
-        </Contrast>
-      ))}
-    </div>
+    <huet.ThemeContext.Provider value={debouncedCtx}>
+      <div>
+        {graySteps.map(grayStep => (
+          <Contrast key={grayStep} bg={grayStep} className="pa2">
+            <b>{grayStep}</b>
+            <div className="flex items-center flex-wrap">
+              {Object.keys(debouncedCtx.ramps).map(ramp => (
+                <div key={ramp} className="flex mt1 mr2">
+                  {colorSteps.map(colorStep => (
+                    <Contrast
+                      key={colorStep}
+                      bg={colorStep}
+                      bgRamp={ramp}
+                      className="pa1"
+                    >
+                      {colorStep.toString()}
+                    </Contrast>
+                  ))}
+                </div>
+              ))}
+            </div>
+          </Contrast>
+        ))}
+      </div>
+    </huet.ThemeContext.Provider>
   );
 }
