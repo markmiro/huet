e454907f273155c57bc58083df0d70f0e3ffb723
Mark Mironyuk <contact@markmiro.com>
2018-12-15 03:54:07 -0800

    Improve performance of ExampleColorContrast

--- src/ExampleColorContrast.js
+++ src/ExampleColorContrast.js
@@ -40,83 +40,84 @@ const stepSizes = {
   }
 };
 
+const Things = React.memo(({ colorSteps, graySteps, ctx }) => (
+  <huet.ThemeContext.Provider value={ctx}>
+    {graySteps.map(grayStep => (
+      <Contrast key={grayStep} bg={grayStep} className="pa2">
+        <b>{grayStep}</b>
+        <div className="flex items-center flex-wrap">
+          {Object.keys(ctx.ramps).map(ramp => (
+            <div key={ramp} className="flex mt1 mr2">
+              {ctx.ramps[ramp].mode === "direct" ? (
+                <Contrast
+                  bg={0}
+                  bgRamp={ramp}
+                  text={0}
+                  textRamp={ramp}
+                  className="pa1"
+                >
+                  Direct
+                </Contrast>
+              ) : (
+                colorSteps.map(colorStep => (
+                  <Contrast
+                    key={colorStep}
+                    bg={colorStep}
+                    bgRamp={ramp}
+                    text={colorStep}
+                    textRamp={ramp}
+                    className="pa1"
+                  >
+                    {colorStep.toString()}
+                  </Contrast>
+                ))
+              )}
+              <div className="flex flex-column">
+                <div
+                  className="w1 h-100"
+                  style={{
+                    backgroundColor: ctx.ramps[ramp].scale(
+                      ctx.maxColorLightness / 100
+                    )
+                  }}
+                />
+                <div
+                  className="w1 h-100"
+                  style={{
+                    backgroundColor: ctx.ramps[ramp].scale(
+                      ctx.minColorLightness / 100
+                    )
+                  }}
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </Contrast>
+    ))}
+  </huet.ThemeContext.Provider>
+));
+
 export default function ColorContrast() {
   const ctx = huet.useTheme();
   const debouncedCtx = useDebounce(ctx.contextValue, 100);
-
   const [stepKey, setStepKey] = useBrowserState("increment20");
 
-  const { colorSteps, graySteps } = stepSizes[stepKey];
-
   return (
-    <huet.ThemeContext.Provider value={debouncedCtx}>
-      <div>
-        <Select
-          label="Step sizes"
-          value={stepKey}
-          onChange={setStepKey}
-          className="pa2"
-        >
-          <option value="doubleFraction">100/16, 100/8, 100/4...</option>
-          <option value="double">0, 4, 8, ..., 92, 96, 100</option>
-          <option value="littleSteps">2, 4, ..., 98, 100</option>
-          <option value="increment10">10, 20, 30, ...</option>
-          <option value="increment20">20, 40, 80, ...</option>
-        </Select>
-        {graySteps.map(grayStep => (
-          <Contrast key={grayStep} bg={grayStep} className="pa2">
-            <b>{grayStep}</b>
-            <div className="flex items-center flex-wrap">
-              {Object.keys(ctx.contextValue.ramps).map(ramp => (
-                <div key={ramp} className="flex mt1 mr2">
-                  {ctx.contextValue.ramps[ramp].mode === "direct" ? (
-                    <Contrast
-                      bg={0}
-                      bgRamp={ramp}
-                      text={0}
-                      textRamp={ramp}
-                      className="pa1"
-                    >
-                      Direct
-                    </Contrast>
-                  ) : (
-                    colorSteps.map(colorStep => (
-                      <Contrast
-                        key={colorStep}
-                        bg={colorStep}
-                        bgRamp={ramp}
-                        text={colorStep}
-                        textRamp={ramp}
-                        className="pa1"
-                      >
-                        {colorStep.toString()}
-                      </Contrast>
-                    ))
-                  )}
-                  <div className="flex flex-column">
-                    <div
-                      className="w1 h-100"
-                      style={{
-                        backgroundColor: ctx.contextValue.ramps[ramp].scale(
-                          ctx.contextValue.maxColorLightness / 100
-                        )
-                      }}
-                    />
-                    <div
-                      className="w1 h-100"
-                      style={{
-                        backgroundColor: ctx.contextValue.ramps[ramp].scale(
-                          ctx.contextValue.minColorLightness / 100
-                        )
-                      }}
-                    />
-                  </div>
-                </div>
-              ))}
-            </div>
-          </Contrast>
-        ))}
-      </div>
-    </huet.ThemeContext.Provider>
+    <div>
+      <Select
+        label="Step sizes"
+        value={stepKey}
+        onChange={setStepKey}
+        className="pa2"
+      >
+        <option value="doubleFraction">100/16, 100/8, 100/4...</option>
+        <option value="double">0, 4, 8, ..., 92, 96, 100</option>
+        <option value="littleSteps">2, 4, ..., 98, 100</option>
+        <option value="increment10">10, 20, 30, ...</option>
+        <option value="increment20">20, 40, 80, ...</option>
+      </Select>
+      <Things {...stepSizes[stepKey]} ctx={debouncedCtx} />
+    </div>
   );
 }
