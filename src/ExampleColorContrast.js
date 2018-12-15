b2c8706746b23b44975f97ecaa5d784b6795118b
Mark Mironyuk <contact@markmiro.com>
2018-12-14 18:51:28 -0800

    Allow picking step size in color contrast demo

--- src/ExampleColorContrast.js
+++ src/ExampleColorContrast.js
@@ -1,31 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import range from "lodash/range";
 import huet from "./huet";
 import Contrast from "./Contrast";
 import { useDebounce } from "use-debounce";
+import Select from "./Select";
+
+const sharedDoubleFractionSteps = [100 / 16, 100 / 8, 100 / 4, 100 / 2];
+const doubleSteps = [4, 8, 16, 32, 64, 100];
+const littleSteps = [0, 2, 4, 6, 96, 98, 100];
+
+const stepSizes = {
+  doubleFraction: {
+    graySteps: [
+      0,
+      ...sharedDoubleFractionSteps,
+      100 - 100 / 4,
+      100 - 100 / 8,
+      100 - 100 / 16,
+      100
+    ],
+    colorSteps: [0, ...sharedDoubleFractionSteps, 100]
+  },
+  double: {
+    graySteps: doubleSteps,
+    colorSteps: doubleSteps
+  },
+  littleSteps: {
+    graySteps: littleSteps,
+    colorSteps: littleSteps
+  },
+  increment10: {
+    graySteps: range(0, 110, 10),
+    colorSteps: range(0, 110, 10)
+  },
+  increment20: {
+    graySteps: range(0, 120, 20),
+    colorSteps: range(0, 120, 20)
+  }
+};
 
 export default function ColorContrast() {
   const ctx = huet.useTheme();
   const debouncedCtx = useDebounce(ctx.contextValue, 100);
-  const sharedSteps = [100 / 16, 100 / 8, 100 / 4, 100 / 2];
-  // const colorSteps = [0, ...sharedSteps, 100];
-  // // const colorSteps = [4, 8, 16, 32, 64, 100];
-  // // const colorSteps = [0, 4, 8, 16, 84, 92, 96, 100];
-  // const graySteps = [
-  //   0,
-  //   ...sharedSteps,
-  //   100 - 100 / 4,
-  //   100 - 100 / 8,
-  //   100 - 100 / 16,
-  //   100
-  // ];
 
-  const colorSteps = range(0, 120, 20);
-  const graySteps = colorSteps;
+  const [stepKey, setStepKey] = useState("increment20");
+
+  const { colorSteps, graySteps } = stepSizes[stepKey];
 
   return (
     <huet.ThemeContext.Provider value={debouncedCtx}>
       <div>
+        <Select
+          label="Step sizes"
+          value={stepKey}
+          onChange={setStepKey}
+          className="pa2"
+        >
+          <option value="doubleFraction">100/16, 100/8, 100/4...</option>
+          <option value="double">0, 4, 8, ..., 92, 96, 100</option>
+          <option value="littleSteps">2, 4, ..., 98, 100</option>
+          <option value="increment10">10, 20, 30, ...</option>
+          <option value="increment20">20, 40, 80, ...</option>
+        </Select>
         {graySteps.map(grayStep => (
           <Contrast key={grayStep} bg={grayStep} className="pa2">
             <b>{grayStep}</b>
