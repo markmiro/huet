b91eda50c3da23d8299bc42f528501a90854119e
Mark Mironyuk <contact@markmiro.com>
2019-01-16 00:32:44 -0800

    Put React stuff in Huet export

--- src/huet.js
+++ src/huet.js
@@ -1,2 +1,9 @@
 export { default as Theme } from "./Theme";
 export { default as Color, getLightness } from "./Color";
+export {
+  ThemeContext,
+  BackgroundContext
+} from "./unstable/private/reactContexts";
+export { default as Block } from "./unstable/Block.jsx";
+export { default as Contrast } from "./unstable/Contrast.jsx";
+export { default as Themer } from "./unstable/Themer.jsx";
