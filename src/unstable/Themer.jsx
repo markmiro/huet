bae87747d8b6f91fc9cbd5c3ac17a2380214d7b5
Mark Mironyuk <contact@markmiro.com>
2019-01-22 15:01:41 -0800

    Embed css in Themer

--- src/unstable/Themer.jsx
+++ src/unstable/Themer.jsx
@@ -13,6 +13,10 @@ import Checkbox from "./private/Checkbox";
 import ColorRamp from "./private/ColorRamp";
 import Pallet from "./private/Pallet";
 
+import "./Themer.css";
+import "tachyons/css/tachyons.css";
+import "../demo/styles.css";
+
 export default function Themer({
   themeConfigs,
   themeConfig,
@@ -83,148 +87,159 @@ export default function Themer({
 
   return (
     <ThemeContext.Provider value={themerTheme}>
-      <Contrast
-        className="Themer f7 flex flex-column"
-        outline={20}
-        bg={0}
-        style={{
-          outlineWidth: 1,
-          outlineStyle: "solid",
-          boxShadow: `0 5px 30px ${themerTheme.ramps.gray(0)}`,
-          position: "fixed",
-          bottom: 0,
-          right: 0,
-          maxHeight: isExpanded ? "100vh" : "20vh",
-          zIndex: 9999999,
-          width: "30em"
-        }}
-      >
+      <div className="Themer">
         <Contrast
-          bg={100}
-          className="db flex justify-between"
+          className="f7 flex flex-column"
+          outline={20}
+          bg={0}
           style={{
-            flexShrink: 0,
-            userSelect: "none"
+            outlineWidth: 1,
+            outlineStyle: "solid",
+            boxShadow: `0 5px 30px ${themerTheme.ramps.gray(0)}`,
+            position: "fixed",
+            bottom: 0,
+            right: 0,
+            maxHeight: isExpanded ? "100vh" : "20vh",
+            zIndex: 9999999,
+            width: "30em"
           }}
-          onClick={() => setIsExpanded(v => !v)}
         >
-          <div className="pv2 ph2 b">Huet Themer (alpha)</div>
-          <Contrast bg={20} className="flex justify-center items-center ph3 b">
-            {isExpanded ? "↓" : "↑"}
-          </Contrast>
-        </Contrast>
-        <div className="overflow-y-scroll overflow-x-hidden">
-          <Contrast bg={10} className="pa2">
-            <div className="flex justify-end items-end flex-wrap">
-              <Select
-                label="Theme"
-                value={themeKey}
-                onChange={setThemeKey}
-                className="flex-auto"
-              >
-                {Object.keys(themeConfigs).map(key => (
-                  <option key={key} value={key}>
-                    {themeConfigs[key].name}
-                  </option>
-                ))}
-              </Select>
-              <ButtonGroup className="mt1 ml1">
-                {isThemeModified && <Button onClick={resetTheme}>Reset</Button>}
-                <Button onClick={exportTheme}>Export</Button>
-                <Button onClick={importTheme}>Import</Button>
-              </ButtonGroup>
-            </div>
-            <Contrast border={20} className="bb mv2" />
-            <Range
-              label="Page background lightness"
-              min={0}
-              max={1}
-              decimals={2}
-              value={themeConfig.bgRampValue}
-              onChange={setBgRampValue}
-            />
-          </Contrast>
-          <div className="pa2">
-            <div className="mb1">Pallet</div>
-            <Pallet colors={themeConfig.pallet} onColorsChange={setPallet} />
-            <div className="mt2 mb1">Color ramps</div>
-            <div className="flex flex-wrap mt1">
-              <div className="w-100">
-                <Contrast
-                  border={10}
-                  className="w-100"
-                  style={{
-                    height: "4px",
-                    marginBottom: 6,
-                    background: "linear-gradient(to right, black, white)"
-                  }}
-                />
-                {Object.keys(theme.ramps).map(key => (
-                  <ColorRamp key={key} ramp={theme.ramps[key]} theme={theme} />
-                ))}
-              </div>
-              <Contrast className="w-100" border={20}>
-                <Range
-                  label="Dark color min lightness"
-                  min={0}
-                  max={100}
-                  value={themeConfig.minColorLightness}
-                  onChange={setMinColorLightness}
-                  className="mt2"
-                />
-                <Range
-                  label="Light color max lightness"
-                  min={0}
-                  max={100}
-                  value={themeConfig.maxColorLightness}
-                  onChange={setMaxColorLightness}
-                  className="mt2"
-                />
-                <Range
-                  label="Lightness contrast multiplier"
-                  min={0}
-                  max={2}
-                  decimals={2}
-                  value={themeConfig.contrastMultiplier}
-                  onChange={setContrastMultiplier}
-                  className="mt2"
-                />
-                <Range
-                  label="Saturation multiplier"
-                  min={0}
-                  max={2}
-                  decimals={2}
-                  value={themeConfig.saturationContrastMultiplier}
-                  onChange={setSaturationContrastMultiplier}
-                  className="mt2"
-                />
-                <Checkbox
-                  label="Rescale gray contrast to gray range"
-                  isChecked={themeConfig.rescaleContrastToGrayRange}
-                  onChange={setRescaleContrastToGrayRange}
-                  className="mt2"
-                />
-              </Contrast>
-            </div>
-          </div>
-          <Contrast className="bt pa2 flex justify-between" border={10}>
-            <Checkbox
-              label="Theme the themer"
-              isChecked={shouldThemeSelf}
-              onChange={setShouldThemeSelf}
-            />
-            <Button
-              bg={50}
-              bgRamp="red"
-              textRamp="white"
-              onClick={reset}
-              verify
+          <Contrast
+            bg={100}
+            className="db flex justify-between"
+            style={{
+              flexShrink: 0,
+              userSelect: "none"
+            }}
+            onClick={() => setIsExpanded(v => !v)}
+          >
+            <div className="pv2 ph2 b">Huet Themer (alpha)</div>
+            <Contrast
+              bg={20}
+              className="flex justify-center items-center ph3 b"
             >
-              Reset Settings
-            </Button>
+              {isExpanded ? "↓" : "↑"}
+            </Contrast>
           </Contrast>
-        </div>
-      </Contrast>
+          <div className="overflow-y-scroll overflow-x-hidden">
+            <Contrast bg={10} className="pa2">
+              <div className="flex justify-end items-end flex-wrap">
+                <Select
+                  label="Theme"
+                  value={themeKey}
+                  onChange={setThemeKey}
+                  className="flex-auto"
+                >
+                  {Object.keys(themeConfigs).map(key => (
+                    <option key={key} value={key}>
+                      {themeConfigs[key].name}
+                    </option>
+                  ))}
+                </Select>
+                <ButtonGroup className="mt1 ml1">
+                  {isThemeModified && (
+                    <Button onClick={resetTheme}>Reset</Button>
+                  )}
+                  <Button onClick={exportTheme}>Export</Button>
+                  <Button onClick={importTheme}>Import</Button>
+                </ButtonGroup>
+              </div>
+              <Contrast border={20} className="bb mv2" />
+              <Range
+                label="Page background lightness"
+                min={0}
+                max={1}
+                decimals={2}
+                value={themeConfig.bgRampValue}
+                onChange={setBgRampValue}
+              />
+            </Contrast>
+            <div className="pa2">
+              <div className="mb1">Pallet</div>
+              <Pallet colors={themeConfig.pallet} onColorsChange={setPallet} />
+              <div className="mt2 mb1">Color ramps</div>
+              <div className="flex flex-wrap mt1">
+                <div className="w-100">
+                  <Contrast
+                    border={10}
+                    className="w-100"
+                    style={{
+                      height: "4px",
+                      marginBottom: 6,
+                      background: "linear-gradient(to right, black, white)"
+                    }}
+                  />
+                  {Object.keys(theme.ramps).map(key => (
+                    <ColorRamp
+                      key={key}
+                      ramp={theme.ramps[key]}
+                      theme={theme}
+                    />
+                  ))}
+                </div>
+                <Contrast className="w-100" border={20}>
+                  <Range
+                    label="Dark color min lightness"
+                    min={0}
+                    max={100}
+                    value={themeConfig.minColorLightness}
+                    onChange={setMinColorLightness}
+                    className="mt2"
+                  />
+                  <Range
+                    label="Light color max lightness"
+                    min={0}
+                    max={100}
+                    value={themeConfig.maxColorLightness}
+                    onChange={setMaxColorLightness}
+                    className="mt2"
+                  />
+                  <Range
+                    label="Lightness contrast multiplier"
+                    min={0}
+                    max={2}
+                    decimals={2}
+                    value={themeConfig.contrastMultiplier}
+                    onChange={setContrastMultiplier}
+                    className="mt2"
+                  />
+                  <Range
+                    label="Saturation multiplier"
+                    min={0}
+                    max={2}
+                    decimals={2}
+                    value={themeConfig.saturationContrastMultiplier}
+                    onChange={setSaturationContrastMultiplier}
+                    className="mt2"
+                  />
+                  <Checkbox
+                    label="Rescale gray contrast to gray range"
+                    isChecked={themeConfig.rescaleContrastToGrayRange}
+                    onChange={setRescaleContrastToGrayRange}
+                    className="mt2"
+                  />
+                </Contrast>
+              </div>
+            </div>
+            <Contrast className="bt pa2 flex justify-between" border={10}>
+              <Checkbox
+                label="Theme the themer"
+                isChecked={shouldThemeSelf}
+                onChange={setShouldThemeSelf}
+              />
+              <Button
+                bg={50}
+                bgRamp="red"
+                textRamp="white"
+                onClick={reset}
+                verify
+              >
+                Reset Settings
+              </Button>
+            </Contrast>
+          </div>
+        </Contrast>
+      </div>
     </ThemeContext.Provider>
   );
 }
