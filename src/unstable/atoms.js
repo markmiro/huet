9bf531ac084693f6768aee66c9ebe58a1f24f370
Mark Mironyuk <contact@markmiro.com>
2019-01-23 11:38:19 -0800

    Add notes to atoms

--- src/unstable/atoms.js
+++ src/unstable/atoms.js
@@ -1,5 +1,51 @@
 import { make, makeSides, chainable } from "./atom-helpers";
-// Adapted from: https://tachyons.io/docs/table-of-styles/
+/*
+  Adapted from: https://tachyons.io/docs/table-of-styles/
+  Adding styles as they're used to limit bloat.
+
+  Why this exists?
+  ---
+  I wanted to use Tachyon's atomic styles in this components because it's easier to
+  quickly style individual elements with it and prototype. However, it's so nice to use
+  atomic styles that when it came to bundling, the <Themer /> so it can be embedded in
+  other projects, I decided to opt for keeping atomic styles rather than rewriting them
+  into something else and figuring out how to name all the class names and so on.
+
+  I would prefer to keep atomic styles in classes, but:
+  - I don't want to pollute the global namespance of anyone who uses the <Themer /> component
+  - I couldn't find any packages that might convert or namespace the CSS (to minimize collisions)
+  - Doing the conversion myself via Babel would have been tricky
+  - It seems easier to generate classes if the styles are already in JS. Then again, the current approach
+    of adding atoms to the style prop may not be the best idea. Maybe add them to a custom prop
+    like `atomStyle` or `atoms` and then transpile it into namespaced classes.
+
+  Why not use nano-css?
+  - It didnt' include all the Tachyon styles I wanted, and included ones that I didn't want
+  - Fun to rebuild the `snake` addon functionality to understand how it works
+  - Keep tabs of which atoms I tend to use in one place
+  - I may end up switching to nano-css anyways
+
+  Why underscores?
+  ---
+  It's easier to read, though not idiomatic JS.
+
+  Limitations
+  ---
+  Putting styles on the style prop in React makes them difficult to override. However, for components
+  meant to be embedded in other apps, it also prevents others from easily overriding styles.
+
+  Future explorations
+  ---
+  It will be interesting to explore treating the style prop as the real primitive that we want to target
+  and then think of conversion to classes as an optimization. Even so, we still want to style
+  hover effects, pseudo form elements, etc.
+
+  Ideas:
+  - Add Huet color support
+  - Add some layout styles
+  - Put styles on custom React prop `atoms` and then extract CSS at build time
+    (Huet colors may not be extractable)
+*/
 
 // EX: `mt2` will use spaceScale[2]
 const fontScale = [
