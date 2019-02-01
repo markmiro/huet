6aec52adcf73b094db8ddfedac2db6209a34fcd5
Mark Mironyuk <contact@markmiro.com>
2019-01-31 16:19:41 -0800

    Comment out generated pallet and use RGB cube corner colors instead

--- src/private/baseThemeConfig.js
+++ src/private/baseThemeConfig.js
@@ -1,23 +1,34 @@
-import chroma from "chroma-js";
+// import chroma from "chroma-js";
 /*
   Maybe we can allow the pallet to be generated:
   - Pick min and max lightness
   - Pick base color
 */
 
-const hueStart = 20; // Gets us blues and greens that have 0 in the R in RGB
-const l = 50; // Doesn't really matter which value we put here
-const c = 40; // Over about 45 leads to colors in blue range to
-const colorLabels = ["red", "gold", "green" /*skip teal*/, , "blue", "purple"];
-const hueUnit = 360 / colorLabels.length;
+// const hueStart = 20; // Gets us blues and greens that have 0 in the R in RGB
+// const l = 50; // Doesn't really matter which value we put here
+// const c = 40; // Over about 45 leads to colors in blue range to
+// const colorLabels = ["red", "gold", "green" /*skip teal*/, , "blue", "purple"];
+// const hueUnit = 360 / colorLabels.length;
 
-const palletColors = colorLabels.reduce(
-  (acc, label, i) => ({
-    ...acc,
-    [label]: chroma.hcl(hueUnit * i + hueStart, c, l).hex()
-  }),
-  {}
-);
+// const palletColors = colorLabels.reduce(
+//   (acc, label, i) => ({
+//     ...acc,
+//     [label]: chroma.hcl(hueUnit * i + hueStart, c, l).hex()
+//   }),
+//   {}
+// );
+
+const palletColors = {
+  red: "#ff0000",
+  yellow: "#ffff00",
+  green: "#00ff00",
+  cyan: "#00ffff",
+  blue: "#0000ff",
+  magenta: "#ff00ff"
+};
+
+const colorLabels = Object.keys(palletColors);
 
 const ramps = colorLabels.reduce(
   (acc, label) => ({
@@ -36,6 +47,7 @@ export default {
   },
   ramps: {
     gray: {
+      colorModel: "hcl",
       isNeutral: true
     },
     white: {
