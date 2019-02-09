817fe875607186f56e365ba5c58ef5f12f2815a9
Mark Mironyuk <contact@markmiro.com>
2019-02-09 14:48:14 -0800

    Tweak example themes

--- src/demo/themeConfigs.js
+++ src/demo/themeConfigs.js
@@ -1,36 +1,36 @@
 import shared from "../private/baseThemeConfig.js";
 
-function rgbCubeTheme(shared) {
-  const palletColors = {
-    red: "#ff0000",
-    yellow: "#ffff00",
-    green: "#00ff00",
-    cyan: "#00ffff",
-    blue: "#0000ff",
-    purple: "#ff00ff"
-  };
+// function rgbCubeTheme(shared) {
+//   const palletColors = {
+//     red: "#ff0000",
+//     yellow: "#ffff00",
+//     green: "#00ff00",
+//     cyan: "#00ffff",
+//     blue: "#0000ff",
+//     purple: "#ff00ff"
+//   };
 
-  const ramps = Object.keys(palletColors).reduce(
-    (acc, label) => ({
-      ...acc,
-      [label]: { colors: ["black", label, "white"] }
-    }),
-    {}
-  );
+//   const ramps = Object.keys(palletColors).reduce(
+//     (acc, label) => ({
+//       ...acc,
+//       [label]: { colors: ["black", label, "white"] }
+//     }),
+//     {}
+//   );
 
-  return {
-    ...shared,
-    name: "RGB Cube Corners",
-    pallet: {
-      ...shared.pallet,
-      ...palletColors
-    },
-    ramps: {
-      ...shared.ramps,
-      ...ramps
-    }
-  };
-}
+//   return {
+//     ...shared,
+//     name: "RGB Cube Corners",
+//     pallet: {
+//       ...shared.pallet,
+//       ...palletColors
+//     },
+//     ramps: {
+//       ...shared.ramps,
+//       ...ramps
+//     }
+//   };
+// }
 
 const themes = [
   {
@@ -41,40 +41,63 @@ const themes = [
     name: "Base Inverted",
     bgRampValue: 0
   },
-  rgbCubeTheme(shared),
+  // rgbCubeTheme(shared),
+  // {
+  //   ...shared,
+  //   name: "High Contrast",
+  //   pallet: {
+  //     ...shared.pallet,
+  //     red: "#ff0030",
+  //     yellow: "#b77a00",
+  //     green: "#009700",
+  //     blue: "#0099ff",
+  //     purple: "#d400ff"
+  //   },
+  //   bgRampValue: 0,
+  //   startSignalLightness: 0.3,
+  //   endSignalLightness: 0.7,
+  //   contrastMultiplier: 2,
+  //   saturationMultiplier: 2
+  // },
+  // {
+  //   ...shared,
+  //   name: "Radioactive",
+  //   pallet: {
+  //     ...shared.pallet,
+  //     black: "#69707f",
+  //     white: "#c1ff00",
+  //     red: "#ff88a4",
+  //     yellow: "#b1bd2d",
+  //     green: "#59c866",
+  //     blue: "#83beff",
+  //     purple: "#d2a8ff"
+  //   },
+  //   bgRampValue: 0,
+  //   startSignalLightness: 0.46,
+  //   endSignalLightness: 0.61
+  // },
   {
     ...shared,
-    name: "High Contrast",
+    name: "Blueprint",
     pallet: {
       ...shared.pallet,
+      black: "#384cd2",
+      white: "#ffffff",
       red: "#ff0030",
-      yellow: "#b77a00",
+      yellow: "#988753",
       green: "#009700",
-      blue: "#0099ff",
-      purple: "#d400ff"
+      blue: "#00a3ff",
+      purple: "#af60ff"
     },
-    bgRampValue: 0,
-    startSignalLightness: 0.3,
-    endSignalLightness: 0.7,
-    contrastMultiplier: 2,
-    saturationMultiplier: 2
-  },
-  {
-    ...shared,
-    name: "Radioactive",
-    pallet: {
-      ...shared.pallet,
-      black: "#69707f",
-      white: "#c1ff00",
-      red: "#ff88a4",
-      yellow: "#b1bd2d",
-      green: "#59c866",
-      blue: "#83beff",
-      purple: "#d2a8ff"
+    ramps: {
+      ...shared.ramps,
+      gray: {
+        ...shared.ramps.gray,
+        colorModel: "lrgb"
+      }
     },
-    bgRampValue: 0,
-    startSignalLightness: 0.46,
-    endSignalLightness: 0.61
+    startSignalLightness: 0.26,
+    endSignalLightness: 0.47
   },
   {
     ...shared,
@@ -91,23 +114,6 @@ const themes = [
     },
     bgRampValue: 0,
     startSignalLightness: 0.5,
-    endSignalLightness: 0.63
-  },
-  {
-    ...shared,
-    name: "Deep Dive",
-    pallet: {
-      ...shared.pallet,
-      black: "#001b4f",
-      white: "#b6e8e8",
-      red: "#ff6669",
-      yellow: "#a09c5c",
-      green: "#319647",
-      blue: "#496eff",
-      purple: "#af5bff"
-    },
-    bgRampValue: 0,
-    startSignalLightness: 0.44,
     endSignalLightness: 0.63
   },
   {
@@ -129,21 +135,38 @@ const themes = [
   },
   {
     ...shared,
-    name: "Royal Purple",
+    name: "Deep Dive",
     pallet: {
       ...shared.pallet,
-      black: "#37233b",
-      white: "#f0c9e7",
-      red: "#e13639",
-      yellow: "#bd7f33",
-      green: "#00878e",
-      blue: "#3865da",
-      purple: "#7630c8"
+      black: "#001b4f",
+      white: "#b6e8e8",
+      red: "#ff6669",
+      yellow: "#a09c5c",
+      green: "#319647",
+      blue: "#496eff",
+      purple: "#af5bff"
     },
     bgRampValue: 0,
-    startSignalLightness: 0.5,
-    endSignalLightness: 0.5
+    startSignalLightness: 0.44,
+    endSignalLightness: 0.63
   },
+  // {
+  //   ...shared,
+  //   name: "Royal Purple",
+  //   pallet: {
+  //     ...shared.pallet,
+  //     black: "#37233b",
+  //     white: "#f0c9e7",
+  //     red: "#e13639",
+  //     yellow: "#bd7f33",
+  //     green: "#00878e",
+  //     blue: "#3865da",
+  //     purple: "#7630c8"
+  //   },
+  //   bgRampValue: 0,
+  //   startSignalLightness: 0.5,
+  //   endSignalLightness: 0.5
+  // },
   {
     ...shared,
     name: "Orange Boost",
@@ -163,41 +186,25 @@ const themes = [
     contrastMultiplier: 1,
     saturationMultiplier: 0.83
   },
-  {
-    ...shared,
-    name: "Faded Future",
-    pallet: {
-      ...shared.pallet,
-      black: "#3c3d43",
-      white: "#b2bdff",
-      red: "#ff3200",
-      yellow: "#987a1d",
-      green: "#459700",
-      blue: "#0073ff",
-      purple: "#aa5dff"
-    },
-    bgRamp: "gray",
-    bgRampValue: 0,
-    startSignalLightness: 0.36,
-    endSignalLightness: 0.72,
-    saturationMultiplier: 0.59
-  },
-  {
-    ...shared,
-    name: "Blueprint",
-    pallet: {
-      ...shared.pallet,
-      black: "#384cd2",
-      white: "#ffffff",
-      red: "#ff0030",
-      yellow: "#988753",
-      green: "#009700",
-      blue: "#00a3ff",
-      purple: "#af60ff"
-    },
-    startSignalLightness: 0.26,
-    endSignalLightness: 0.47
-  },
+  // {
+  //   ...shared,
+  //   name: "Faded Future",
+  //   pallet: {
+  //     ...shared.pallet,
+  //     black: "#3c3d43",
+  //     white: "#b2bdff",
+  //     red: "#ff3200",
+  //     yellow: "#987a1d",
+  //     green: "#459700",
+  //     blue: "#0073ff",
+  //     purple: "#aa5dff"
+  //   },
+  //   bgRamp: "gray",
+  //   bgRampValue: 0,
+  //   startSignalLightness: 0.36,
+  //   endSignalLightness: 0.72,
+  //   saturationMultiplier: 0.59
+  // },
   {
     ...shared,
     name: "A Box of Macaroni",
@@ -215,6 +222,56 @@ const themes = [
     endSignalLightness: 0.75,
     saturationMultiplier: 1.5,
     contrastMultiplier: 1.5
+  },
+  // {
+  //   ...shared,
+  //   name: "Sunset",
+  //   pallet: {
+  //     ...shared.pallet,
+  //     black: "#242744",
+  //     white: "#ffe173",
+  //     red: "#f20031",
+  //     yellow: "#c16000",
+  //     green: "#008d29",
+  //     blue: "#12889d",
+  //     purple: "#b555c8"
+  //   },
+  //   ramps: {
+  //     ...shared.ramps,
+  //     gray: {
+  //       ...shared.ramps.gray,
+  //       colorModel: "hcl"
+  //     }
+  //   },
+  //   bgRampValue: 0,
+  //   startSignalLightness: 0.42,
+  //   endSignalLightness: 0.58
+  // },
+  {
+    ...shared,
+    name: "Astro",
+    pallet: {
+      ...shared.pallet,
+      black: "#040816",
+      gray: "#ffb980",
+      white: "#ffffff",
+      red: "#cb1c00",
+      yellow: "#bd8f5a",
+      green: "#02b984",
+      blue: "#4f81a7",
+      purple: "#b982d5"
+    },
+    ramps: {
+      ...shared.ramps,
+      gray: {
+        ...shared.ramps.gray,
+        colors: ["black", "gray", "white"],
+        colorModel: "hcl"
+      }
+    },
+    bgRampValue: 0,
+    startSignalLightness: 0.42,
+    endSignalLightness: 0.58
   }
 ];
 
