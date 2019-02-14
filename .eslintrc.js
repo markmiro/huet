86ba3829f40445ad26acfec8f4d0a7e1ce444271
Mark Mironyuk <contact@markmiro.com>
2019-02-13 19:21:32 -0800

    Add linting

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+// Consider: https://github.com/benmosher/eslint-plugin-import
+// Consider: https://github.com/amilajack/eslint-plugin-compat
+
+module.exports = {
+  parser: "babel-eslint",
+  parserOptions: {
+    // ecmaVersion: "2018", // TODO: check that this needs to be enabled?
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  plugins: ["jsx-a11y", "lodash", "fp", "simple-import-sort"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:unicorn/recommended",
+    "plugin:lodash/canonical",
+    "problems",
+    "prettier"
+  ],
+  rules: {
+    "react/prop-types": "off",
+    "react/no-typos": "on",
+    "react/jsx-handler-names": "on",
+    "react/jsx-no-literals": "on",
+    "react/jsx-no-comment-textnodes": "on",
+    "react/jsx-no-target-blank": "on",
+    "lodash/prefer-lodash-method": "off",
+    "lodash/import-scope": "off", // Using a plugin to do this during build
+    "lodash/prefer-lodash-chain": "off", // Chaining has better aesthetic, but bundle would be bigger?
+    "no-mutating-methods": "on",
+    // TODO: Check these: sort-imports and import/order
+    // https://github.com/lydell/eslint-plugin-simple-import-sort
+    "simple-import-sort/sort": "error",
+
+    // To enable later:
+    "unicorn/filename-case": "off",
+    "unicorn/prefer-query-selector": "off",
+    "react/no-unescaped-entities": "off",
+    "react/display-name": "off"
+  },
+  env: {
+    browser: true,
+    es6: true,
+    jest: true
+    // node: true
+  }
+};
+
+// no-use-before-define
+// no-shadow
